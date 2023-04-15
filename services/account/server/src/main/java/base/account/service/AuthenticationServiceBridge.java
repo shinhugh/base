f5521a0ab904354bccee72c7f13a3e57ef20a95b
Hugh Shin <hughbee135@gmail.com>
2023-04-14 22:17:48 -0700

    Progress on authentication service client

--- services/account/server/src/main/java/base/account/service/AuthenticationServiceBridge.java
+++ services/account/server/src/main/java/base/account/service/AuthenticationServiceBridge.java
@@ -1,11 +1,15 @@
 package base.account.service;
 
+import base.account.service.model.AccessDeniedException;
 import base.account.service.model.Authority;
+import base.account.service.model.IllegalArgumentException;
 
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AuthenticationServiceBridge implements AuthenticationServiceClient {
-    private static final int PORT_MAX_VALUE = 65535;
     private final HttpClient httpClient;
     private final String host;
     private final int port;
@@ -23,12 +27,38 @@ public class AuthenticationServiceBridge implements AuthenticationServiceClient 
     }
 
     @Override
-    public void logout(Authority authority, String accountId) {
+    public void logout(Authority authority, String accountId) throws IllegalArgumentException, AccessDeniedException {
         // TODO: Bridge should not know what's acceptable or not for parameters - leave that up to actual service
         // TODO: Invoke endpoint via HTTP request using httpClient
         // TODO: Anticipate connection problems and invalid host/port values
         //       Categorize these under unexpected exceptions - use wrapException() to wrap exception with custom
         //       message
+        Map<String, List<String>> requestHeaders = new HashMap<>();
+        requestHeaders.put("content-type", List.of("application/json"));
+        if (authority != null) {
+            if (authority.getId() != null) {
+                requestHeaders.put("authority-id", List.of(authority.getId()));
+            }
+            requestHeaders.put("authority-roles", List.of(String.valueOf(authority.getRoles())));
+            requestHeaders.put("authority-auth-time", List.of(String.valueOf(authority.getAuthTime())));
+        }
+        InputStream requestBody = null; // TODO: Map accountId to body as JSON
+        HttpClient.Request request = new HttpClient.Request(host, port, "/logout", HttpClient.Method.POST, requestHeaders, null, requestBody);
+        HttpClient.Response response = httpClient.sendRequest(request);
+        switch (response.getStatus()) {
+            case 200: {
+                return;
+            }
+            case 400: {
+                throw new IllegalArgumentException();
+            }
+            case 401: {
+                throw new AccessDeniedException();
+            }
+            default: {
+                throw new RuntimeException("Unrecognized status code received from authentication service");
+            }
+        }
     }
 
     private static boolean validateConfig(Map<String, String> config) {
@@ -45,6 +75,6 @@ public class AuthenticationServiceBridge implements AuthenticationServiceClient 
         catch (Exception e) {
             return false;
         }
-        return port >= 0 && port <= PORT_MAX_VALUE;
+        return port >= 0;
     }
 }
--- services/account/server/src/main/java/base/account/service/AuthenticationServiceClient.java
+++ services/account/server/src/main/java/base/account/service/AuthenticationServiceClient.java
@@ -1,7 +1,9 @@
 package base.account.service;
 
+import base.account.service.model.AccessDeniedException;
 import base.account.service.model.Authority;
+import base.account.service.model.IllegalArgumentException;
 
 public interface AuthenticationServiceClient {
-    void logout(Authority authority, String accountId);
+    void logout(Authority authority, String accountId) throws IllegalArgumentException, AccessDeniedException;
 }
